44f238d09d800e19a504d5da0cc72fc079d55bf7
NoorBayan <157401879+NoorBayan@users.noreply.github.com>
2026-01-17 01:42:36 +0300

    Create frames.py

---
+++ src/config/frames.py
@@ -0,0 +1,24 @@
+# ------------------------------------------------------------
+#  Quran Ayah Frame Templates
+# ------------------------------------------------------------
+
+QURAN_FRAME_TEMPLATES = {
+    "QuranTemplate_BASE": "The Visual Ayah",
+    "COLOR_BOARD": "Color Learning Board",
+    "QURAN_HEROES": "Quran Heroes",
+
+    "QURAN_GARDEN": "Quran Garden",
+    "ANIMAL_WORLD": "Animal World",
+    "SKY_STARS": "Sky & Stars",
+
+    "WORD_SPLITTER": "Word Splitter",
+    "FLASHCARD": "Educational Flashcard",
+    "AYAH_SONG": "Ayah & Song",
+
+    "DRAW_WITH_AYAH": "Draw with the Ayah",
+    "AYAH_PUZZLE": "Ayah Puzzle",
+    "AYAH_THEATER": "Ayah Theater",
+
+    "CHALLENGE_LEVELS": "Challenge Levels",
+    "MY_LITTLE_MUSHAF": "My Little Mushaf"
+}
