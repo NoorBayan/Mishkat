b63ee4d9a9ace2fec8bd7a4a39c12ec131f45478
NoorBayan <157401879+NoorBayan@users.noreply.github.com>
2026-01-17 01:41:13 +0300

    Create translations.py

---
+++ src/config/translations.py
@@ -0,0 +1,47 @@
+# ------------------------------------------------------------
+# Quran Translation Languages & Translators
+# ------------------------------------------------------------
+
+LANGUAGE_MAP = {
+    "bn": "Bengali - البنغالية - বাংলা",
+    "de": "German - الألمانية - Deutsch",
+    "el": "Greek - اليونانية - Ελληνικά",
+    "en": "English - الإنجليزية - English",
+    "gu": "Gujarati - الغوجاراتية - ગુજરાતી",
+    "hi": "Hindi - الهندية - हिन्दी",
+    "id": "Indonesian - الإندونيسية - Bahasa Indonesia",
+    "iw": "Hebrew - العبرية - עברית",
+    "ja": "Japanese - اليابانية - 日本語",
+    "km": "Khmer - الخميرية - ភាសាខ្មែរ",
+    "ko": "Korean - الكورية - 한국어",
+    "ml": "Malayalam - الماليالامية - മലയാളം",
+    "ms": "Malay - الماليزية - Bahasa Melayu",
+    "rw": "Serbian - الصربية - Српски",
+    "si": "Sinhala - السنهالية - සිංහල",
+    "sq": "Albanian - الألبانية - Shqip",
+    "tr": "Turkish - التركية - Türkçe",
+    "uk": "Ukrainian - الأوكرانية - Українська",
+    "vi": "Vietnamese - الفيتنامية - Tiếng Việt"
+}
+
+TRANSLATION_MAP = {
+    "bn": ["bn_hoque"],
+    "de": ["de_aburida"],
+    "el": ["el_rwwad"],
+    "en": ["en_hilali"],
+    "gu": ["gu_omari"],
+    "hi": ["hi_hindi"],
+    "id": ["id_indonesian"],
+    "iw": ["iw_darussalam"],
+    "ja": ["ja_japanese"],
+    "km": ["km_cambodia"],
+    "ko": ["ko_korean"],
+    "ml": ["ml_abdulhameed"],
+    "ms": ["ms_basmeih"],
+    "rw": ["rw_Serbian"],
+    "si": ["si_mahir"],
+    "sq": ["sq_nahi"],
+    "tr": ["tr_golpinarli"],
+    "uk": ["uk_yakubovych"],
+    "vi": ["vi_rwwad"]
+}
